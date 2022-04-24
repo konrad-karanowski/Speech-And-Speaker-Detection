07b46e83c4e4bbc56b84183218f43150623a2587
Konrad Karanowski <konrad.karanowski@gmail.com>
2022-04-24 02:10:34 +0200

    Added backbone from PAC

--- models/backbones/backbone.py
+++ models/backbones/backbone.py
@@ -47,3 +47,32 @@ class CustomBackbone(Backbone):
         fe = self.fe(x)
         flatt = self.flatten(fe)
         return flatt
+
+
+class CatalunaBackbone(Backbone):
+
+    def __init__(self, input_size: Tuple[int, int, int]) -> None:
+        super(CatalunaBackbone, self).__init__(input_size)
+        self.fe = nn.Sequential(
+            nn.Conv2d(1, 128, (3, 3), (1, 1)),
+            nn.GELU(),
+            nn.Conv2d(128, 128, (3, 3), (1, 1)),
+            nn.GELU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(128, 256, (3, 3), (1, 1)),
+            nn.GELU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(256, 512, (3, 3), (1, 1)),
+            nn.GELU(),
+            nn.Conv2d(512, 512, (3, 3), (1, 1)),
+            nn.GELU(),
+            nn.MaxPool2d(2, 2)
+        )
+        self.flatten = nn.Flatten()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        fe = self.fe(x)
+        flatt = self.flatten(fe)
+        return flatt
+
+
